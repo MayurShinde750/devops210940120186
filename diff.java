09094fbd109fe446b7f75d5e829216f4f8310472
Mayur Shinde <mjshinde0402@gmail.com>
2022-01-30 14:46:41 +0530

    initial commit

---
+++ diff.java
@@ -0,0 +1,8 @@
+public class Diff{
+    public static void main(String[] args){
+        int a=55;
+        int b=20;
+        int diff=a-b;
+        System.out.println("The diff of two no's is ="+diff);
+    }
+}
